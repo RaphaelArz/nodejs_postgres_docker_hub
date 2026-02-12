102693285485a6afa97e94886737dbff17400e2b
Raphael <raphaelarzoin173@gmail.com>
2026-02-12 11:26:14 +0100

    azertyuiop

--- app/models/index.js
+++ app/models/index.js
@@ -1,5 +1,5 @@
 const { Sequelize, DataTypes } = require('sequelize');
-const sequelize = new Sequelize('13.38.112.104:3173', {
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
   logging: false,
 });
